76f3d1fce3a472c41b1cdfd64c22a9a19ed82e43
Jacob Benson <jacob@coffeeandcrypto.xyz>
2022-08-19 16:47:34 -0500

    remix fund me complete!

--- My Exercise Files/Remix Fund Me/FundMe.sol
+++ My Exercise Files/Remix Fund Me/FundMe.sol
@@ -1,2 +1,64 @@
+// Get funds from users
+// Withdraw funds
+// Set a minimum funding value in USD
+
 // SPDX-License-Identifier:  MIT
 pragma solidity ^0.8.8;
+
+import "./PriceConverter.sol";
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    uint256 public constant MINIMUM_USD = 50 * 1e18;
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+    address public immutable i_owner;
+
+    constructor(){
+        i_owner = msg.sender;
+    }
+
+    function fund() public payable {
+        //payable is what turns the button red
+        //smart contracts can hold funds just like wallets
+        //reverting returns the gas fee paid 
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Didn't send enough"); //1e18 = 1*10 **18 = the value in wei (use eth converter tool)
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
+
+    }
+
+    function Withdraw() public onlyOwner {
+        //require(msg.sender == owner, "Sender Is Not Owner!");
+
+        // starting index, ending index, step amount
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex ++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+
+        }
+        //reset the array
+        funders = new address [](0);
+
+        /*//withdraw the funds
+        //transfer
+        payable(msg.sender).transfer(address(this).balance);
+
+        //send
+        bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        require(sendSuccess, "Send Failed");
+        */
+
+        //call
+        (bool callSuccess, ) = payable(msg.sender).call{value:  address(this).balance}("");
+        require(callSuccess, "Call Failed");
+
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == i_owner, "Sender Is Not Owner!");
+        _;
+    }
+}
--- My Exercise Files/Remix Fund Me/PriceConverter.sol
+++ My Exercise Files/Remix Fund Me/PriceConverter.sol
@@ -1,2 +1,35 @@
 // SPDX-License-Identifier:  MIT
+
 pragma solidity ^0.8.8;
+
+    // instructions for this are here: https://docs.chain.link/docs/get-the-latest-price/#solidity
+    // uses the github link and npm to "add" this whole thing to the code without having to add the whole thing
+    import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+
+
+    function GetPrice() internal view returns (uint256) {
+        //we are interacting with a contract outside
+        // ABI
+        // address 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+
+        // this is creating an AggregatorV3Interface variable named priceFeed
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        (,int256 price,,,) = priceFeed.latestRoundData();
+        //this returns the price of ETH in terms of USD
+        return uint256(price * 1e10); // 1**10 == 10000000000
+    }
+
+    function getVersion() internal view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        return priceFeed.version();
+    }
+
+    function getConversionRate(uint256 ethAmount) internal view returns (uint256) {
+        uint256 ethPrice = GetPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+
+}
